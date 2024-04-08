02689fcb9f6958b86aa67d74d8f04f72275c35c4
Henry Solet <hsolet@3G7.42angouleme.fr>
2024-04-08 13:20:31 +0200

    so_long debut

--- fct.c
+++ fct.c
@@ -1,16 +1,37 @@
-void parsing(int fd)
+void parsing(int fd, t_game *s)
 {
-	t_parse *s;
 	int i;
 
 	i = 0;
 	while (get_next_line(fd))
 	{
-		s->line[i] = get_next_line(fd);
+		s->map[i] = ft_split(get_next_line(fd), ' ');
 		i++;
 	}
 }
-int check_map(t_parse *s)
+
+int count_element(t_game *s, int i, int j)
+{
+	if(s->map[i][j] == '0')
+		s->count->ground += 1;
+	else if(s->map[i][j] == '1')
+		s->count->wall += 1;
+	else if(s->map[i][j] == 'P')
+		s->count->P += 1;
+	else if(s->map[i][j] == 'E')
+		s->count->E += 1;
+	else if(s->map[i][j] == 'C')
+		s->count->C += 1;
+}
+void init_struct(t_game *s)
+{
+	s->count->ground = 0;
+	s->count->wall = 0;
+	s->count->P = 0;
+	s->count->E = 0;
+	s->count->C = 0;
+}
+int check_map(t_game *s)
 {
 	int	i;
 	int j;
@@ -18,12 +39,12 @@ int check_map(t_parse *s)
 
 	i = 0;
 	k = 0;
-	while (s->line[i])
+	while (s->map[i])
 	{
 		j = 0;
-		while (s->line[i][j])
+		while (s->map[i][j])
 		{
-			if (!ft_strncmp(s->line[i][j], "01PEC", 5))
+			if (!ft_strncmp(s->map[i][j], "01PEC", 5))
 			exit_error();
 			j++;
 			s->count += 1;
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: hsolet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 09:47:36 by hsolet            #+#    #+#             */
-/*   Updated: 2024/04/08 12:52:43 by hsolet           ###   ########.fr       */
+/*   Updated: 2024/04/08 13:11:41 by hsolet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@
 
 typedef struct s_coord
 {
-	int	*x;
-	int	*y;
-}	t_coord
+	int	x;
+	int	y;
+}	t_coord;
 
 typedef struct s_count
 {
@@ -32,11 +32,13 @@ typedef struct s_count
 	int E;
 	int wall;
 	int ground;
-}	t_count
+}	t_count;
 
-typedef	struct s_player
+typedef	struct s_game
 {
-
-} t_player
+	char **map;
+	t_count *count;
+	t_coord *coord;
+} t_game;
 
 #endif
